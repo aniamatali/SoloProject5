482a84fd61fa0e31f0a23226e4b1e3351692f495
Alvin Niamatali <aniamatali@icloud.com>
2017-10-13 11:20:22 -0700

    added more styling and corrected a link on form page when clicking clear button

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,49 +5,49 @@ using AddressBook.Models;
 
 namespace AddressBook.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    
+    [HttpGet("/")]
+    public ActionResult Index()
     {
-
-        [HttpGet("/")]
-        public ActionResult Index()
-        {
-          List<Contact> allContacts = Contact.GetAll();
-          return View(allContacts);
-        }
-
-        [HttpGet("/Contacts")]
-        public ActionResult Contacts()
-        {
-          List<Contact> allContacts = Contact.GetAll();
-            return View(allContacts);
-        }
-
-        [HttpGet("/Contacts/new")]
-        public ActionResult ContactForm()
-        {
-        return View();
-        }
-
-        [HttpPost("/Contacts")]
-        public ActionResult AddContact()
-        {
-            Contact newContact = new Contact(Request.Form["new-name"],Request.Form["new-address"],Request.Form["new-number"]);
-            List<Contact> allContacts = Contact.GetAll();
-            return View("Contacts", allContacts);
-        }
-
-        [HttpGet("/Contacts/{id}")]
-        public ActionResult ContactDetail(int id)
-        {
-          Contact ContactMod = Contact.Find(id);
-          return View(ContactMod);
-        }
-
-        [HttpPost("/Contacts/list/clear")]
-        public ActionResult ContactListClear()
-        {
-            Contact.ClearAll();
-            return View();
-        }
+      List<Contact> allContacts = Contact.GetAll();
+      return View(allContacts);
     }
+
+    [HttpGet("/Contacts")]
+    public ActionResult Contacts()
+    {
+      List<Contact> allContacts = Contact.GetAll();
+      return View(allContacts);
+    }
+
+    [HttpGet("/Contacts/new")]
+    public ActionResult ContactForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/Contacts")]
+    public ActionResult AddContact()
+    {
+      Contact newContact = new Contact(Request.Form["new-name"],Request.Form["new-address"],Request.Form["new-number"]);
+      List<Contact> allContacts = Contact.GetAll();
+      return View("Contacts", allContacts);
+    }
+
+    [HttpGet("/Contacts/{id}")]
+    public ActionResult ContactDetail(int id)
+    {
+      Contact ContactMod = Contact.Find(id);
+      return View(ContactMod);
+    }
+
+    [HttpPost("/Contacts/list/clear")]
+    public ActionResult ContactListClear()
+    {
+      Contact.ClearAll();
+      return View();
+    }
+  }
 }
